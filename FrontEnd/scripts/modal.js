236f2f56384f261144bbcedfc0a49e8d710ff735
Phorimavong Christopher <phorimavong.c@gmail.com>
2024-09-02 19:03:50 +0200

    final changes

--- FrontEnd/scripts/modal.js
+++ FrontEnd/scripts/modal.js
@@ -74,7 +74,7 @@ async function displayWorksInModal(works) {
 function deleteWorks() {
   const trashes = document.querySelectorAll(".fa-trash-can");
   trashes.forEach((trash) => {
-    trash.addEventListener("click", function (event) {
+    trash.addEventListener("click", function (event) { // ajout event à chaque trash
       
 
       const id = event.target.dataset.id; // récupère l'ID depuis le dataset
@@ -106,140 +106,84 @@ function deleteWorks() {
 }
 
 
-// OUVERTURE & FERMETURE MODAL
+// Fonction pour ouvrir la modale
+function openModal() {
+  document.body.classList.add('modal-open'); // Empêche le défilement de la page lorsque la modale est ouverte
+  modal.showModal();
+  displayWorksInModal(data.works); // Affiche la galerie des travaux dans la modale
+}
+
+// Fonction pour fermer la modale
+function closeModal() {
+  document.body.classList.remove('modal-open'); // Réactive le défilement de la page lorsque la modale est fermée
+  modal.close();
+}
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Créer la modale
   createModal();
 
-  // Ouverture de la modale lorsqu'on clique sur le bouton "Modifier"
-  document.getElementById("edit-projects").onclick = function () {
-    document.body.classList.add('modal-open'); // empêche le défilement
-    modal.showModal();
-  // On affiche la galerie.
-  displayWorksInModal(data.works);
-};
-  // fermeture de la modale
-  closeButton.onclick = () => {
-    document.body.classList.remove('modal-open'); // réactive le défilement
-    modal.close();
-  };
+  // Ouvre la modale lorsqu'on clique sur le bouton "Modifier"
+  document.getElementById("edit-projects").onclick = openModal;
 
-  // fermeture modal lors d'un clic extérieur
+  // Ferme la modale avec la croix
+  closeButton.onclick = closeModal;
+
+  // Ferme la modale sur clic extérieur
   window.addEventListener('click', function(event) {
     if (event.target === modal) {
-      document.body.classList.remove('modal-open');
-      modal.close();
+      closeModal();
     }
-  })
+  });
+
+  // Ferme la modale avec le bouton "close-btn" dans le contenu de la modale
+  document.getElementById('close-btn').addEventListener('click', closeModal);
 });
+
 
 // PASSAGE MODALE 2
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('add-photo').addEventListener('click', secondModal);
 });
 
-
-//MODAL 2
-
-function secondModal() {
-  let modalContent = document.getElementById("dialog-content");
-  modalContent.innerHTML = ''; // vide contenu modal 
-
-  modalContent.innerHTML = `
-  <div id="second-modal-header">
-    <span id="arrow-return"><i class="fa-solid fa-arrow-left"></i></span>
-    <span id="close-btn"><i class="fa-solid fa-xmark"></i></span>
-  </div>
-  <form id="second-modal">
-    <h3>Ajout photo</h3>
-    <div id="form-content"> 
-      <div id="img-upload">
-        <label for="upload-img" class="upload-label">
-        <i class="fa-regular fa-image"></i>
-          <span>+ Ajouter photo</span>
-        </label>
-        <input type="file" id="upload-img" name="image" accept="image/*">
-        <p>jpg, png : 4mo max</p>
+// Fonction pour créer le contenu de la modal 2 
+function secondModalContent() {
+  return `
+    <div id="second-modal-header">
+      <span id="arrow-return"><i class="fa-solid fa-arrow-left"></i></span>
+      <span id="close-btn"><i class="fa-solid fa-xmark"></i></span>
+    </div>
+    <form id="second-modal">
+      <h3>Ajout photo</h3>
+      <div id="form-content"> 
+        <div id="img-upload">
+          <label for="upload-img" class="upload-label">
+          <i class="fa-regular fa-image"></i>
+            <span>+ Ajouter photo</span>
+          </label>
+          <input type="file" id="upload-img" name="image" accept="image/*">
+          <p>jpg, png : 4mo max</p>
+        </div>
+        <label for="work-title">Titre</label>
+        <input type="text" id="work-title" name="title">
+        <label for="work-category">Catégorie</label>
+        <select id="work-category" name="category">
+        </select>
       </div>
-      <label for="work-title">Titre</label>
-      <input type="text" id="work-title" name="title">
-      <label for="work-category">Catégorie</label>
-      <select id="work-category" name="category">
-      </select>
-    </div>
-    <hr id="modal-barre">
-    <div id="submit-add"> 
-      <button type="button" id="submit-btn">Valider</button>
-    </div>
-  </form>
+      <hr id="modal-barre">
+      <div id="submit-add"> 
+        <button type="button" id="submit-btn">Valider</button>
+      </div>
+    </form>
   `;
+}
 
-  /* METHODE AVEC APPENCHILD
-function secondModal() {
-  let modalContent = document.getElementById("dialog-content");
-  modalContent.innerHTML = ''; // vide contenu modal 1
 
-  // création éléments modal 2
-  const form = document.createElement('form');
-  form.id = 'modal-form';
-
-  // Création du bouton pour fermer la modale
-  const arrowReturn = document.createElement("span");
-  arrowReturn.id = "arrow";
-  arrowReturn.innerHTML = '<i class="fa-solid fa-arrow-left"></i>';
-
-  const addPhotoTitle = document.createElement('h3');
-  addPhotoTitle.textContent = 'Ajout photo';
-
-  const addImg = document.createElement('input');
-  addImg.type = 'file';
-  addImg.name = 'image'; 
-  addImg.id = 'upload-img';
-
-  const titleLabel = document.createElement('label');
-  titleLabel.textContent = 'Titre';
-  const titleInput = document.createElement('input');
-  titleInput.type = 'text';
-  titleInput.name = 'title';
-  titleInput.id = 'work-title';
-
-  const categoryLabel = document.createElement('label');
-  categoryLabel.textContent = 'Catégorie';
-  const categorySelect = document.createElement('select');
-  categorySelect.name = 'category';
-  categorySelect.id = 'work-category';
-
-  const validateBtn = document.createElement('button');
-  validateBtn.type = 'button'; 
-  validateBtn.textContent = 'Valider';
-  validateBtn.id = 'submit-add';
-  
-
-  // ajout des éléments à la modal
-  modalContent.appendChild(arrowReturn);
-  modalContent.appendChild(closeButton);
-  form.appendChild(addPhotoTitle);
-  form.appendChild(addImg);
-  form.appendChild(titleLabel);
-  form.appendChild(titleInput);
-  form.appendChild(categoryLabel);
-  form.appendChild(categorySelect);
-  form.appendChild(validateBtn);
-  modalContent.appendChild(form);
-} */
-
-  
-  categoriesOptions(select);
-
-  // ferme modal au clic croix 
-  document.getElementById('close-btn').addEventListener('click', function() {
-    document.body.classList.remove('modal-open'); 
-    document.getElementById('dialog-content').parentElement.close(); 
-  });
-
-  imgInput.addEventListener("change", function() { // ajout event change
-    const file = imgInput.files[0]; // récupère le premier fichier sélectionné
-
+// Fonction pour configurer l'événement de changement de l'image
+function setupImageUpload() {
+  const imgInput = document.getElementById('upload-img');
+  imgInput.addEventListener("change", function() {
+    const file = imgInput.files[0];
     if (file.size > 4 * 1024 * 1024) {
       alert("L'image ne doit pas dépasser 4MB.");
       imgInput.value = null;
@@ -247,29 +191,30 @@ function secondModal() {
       previewImage(file);
     }
   });
+}
 
-
-  const imgInput = document.getElementById('upload-img');
+// Fonction pour gérer la soumission du formulaire
+function formSubmission() {
   const titleInput = document.getElementById('work-title');
   const select = document.getElementById('work-category');
+  const imgInput = document.getElementById('upload-img');
 
   document.getElementById('submit-btn').addEventListener('click', async function(event){
     event.preventDefault();
-    // vérifie que les champs soient bien remplis
-    if (!imgInput.files.length || !titleInput.value.trim || !select.value ) {
-      alert ('Veuillez remplir tous les champs');
+    // si l'un des champs n'est pas remplis
+    if (!imgInput.files.length || !titleInput.value.trim || !select.value) {
+      alert('Veuillez remplir tous les champs');
       return;
     }
 
-    // créer un FormData à envoyer
     const formData = new FormData();
     formData.append('image', imgInput.files[0]);
     formData.append('title', titleInput.value.trim());
     formData.append('category', select.value);
-    
+
     try {
-      const token = localStorage.getItem('token');
-      const response = await fetch ('http://localhost:5678/api/works', {
+      const token = localStorage.getItem('token'); // recupère le token
+      const response = await fetch('http://localhost:5678/api/works', {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${token}`
@@ -279,27 +224,38 @@ function secondModal() {
 
       if (!response.ok) {
         throw new Error('Erreur lors de l\'ajout du travail');
-    }
+      }
 
-    const newWork = await response.json();
-    data.works.push(newWork);
-    displayWorks(data.works);
-    displayWorksInModal(data.works)
-
-    document.getElementById('second-modal').reset(); // Réinitialiser le formulaire
-
+      const newWork = await response.json(); // convertit la reponse en json
+      data.works.push(newWork); // ajout travail à la liste
+      displayWorks(data.works); // gallery maj sur page accueil
+      displayWorksInModal(data.works); // maj dans la modale
+      document.getElementById('second-modal').reset(); // form reinitialisé
+      returnToFirstModal(); 
     } catch (error) {
       console.error('Erreur:', error);
       alert('Une erreur est survenue lors de l\'ajout du travail.');
     }
   });
-  
-  // RETOUR EN ARRIERE MODAL
-  document.getElementById('arrow-return').addEventListener('click', function() {
-    returnToFirstModal(); // fonction pour afficher la première modale
-  });
 }
 
+function secondModal() {
+  let modalContent = document.getElementById("dialog-content");
+  modalContent.innerHTML = secondModalContent();
+  
+  const select = document.getElementById('work-category');
+  categoriesOptions(select);
+
+  document.getElementById('close-btn').addEventListener('click', closeModal);
+
+  document.getElementById('arrow-return').addEventListener('click', returnToFirstModal);
+
+
+  setupImageUpload();
+  checkFormChange();
+  formSubmission();
+}
+  
 
 // FONCTION VISUALISATION IMAGE DANS MODAL 2
 function previewImage(file) {
@@ -312,10 +268,10 @@ function previewImage(file) {
       preview.style.padding = '0'; // enlève le padding
 
       // remplace le contenu du conteneur par l'image
-      preview.innerHTML = ''; // Vide le conteneur
+      preview.innerHTML = ''; // vide le conteneur
       const image = document.createElement('img');
       image.src = event.target.result;
-      preview.appendChild(image); // Ajoute l'image au conteneur
+      preview.appendChild(image); // ajoute l'image au conteneur
     });
 
     reader.readAsDataURL(file); // lit le contenu sous forme d'url de données
@@ -361,3 +317,27 @@ function returnToFirstModal() {
   // Affiche la galerie
   displayWorksInModal(data.works);
 }
+
+function checkFormChange() {
+  const imgInput = document.getElementById('upload-img');
+  const titleInput = document.getElementById('work-title');
+  const select = document.getElementById('work-category');
+  const submitButton = document.getElementById('submit-btn');
+
+  // Surveille les changements dans les champs du formulaire
+  imgInput.addEventListener('change', checkFormFields); // changement si fichier est select ou modifié
+  titleInput.addEventListener('input', checkFormFields);
+  select.addEventListener('change', checkFormFields); // changement lorsque une nouvelle catégorie est selectionné 
+
+  function checkFormFields() {
+    if (imgInput.files.length > 0 && titleInput.value.trim() !== '' && select.value !== '') {
+      submitButton.style.backgroundColor = '1D6154'; // Change la couleur de fond du bouton en vert
+      submitButton.style.color = 'white'; // Change la couleur du texte en blanc pour un meilleur contraste
+    } else {
+      submitButton.style.backgroundColor = ''; // Réinitialise la couleur de fond (revient à la couleur par défaut)
+      submitButton.style.color = ''; // Réinitialise la couleur du texte
+    }
+  }
+}
+
+
